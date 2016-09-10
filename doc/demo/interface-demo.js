f74b5600bb932b8cb1a4d3132da674d4f34cbf43
Ric Wright <rkwright@geofx.com>
2016-09-10 14:11:55 -0500

    Continued to flesh out the demo of the interfaces.  Lots left.

--- doc/demo/interface-demo.js
+++ doc/demo/interface-demo.js
@@ -19,7 +19,7 @@
      */
     var config = [ {"margin":"1em" } ];
 
-    var readingSystem = new ReadingSystem(config);
+    var rs = new ReadingSystem(config);
 
     /*
      *  First initialize the renderer by passing in the container within
@@ -32,15 +32,15 @@
     document.body.appendChild( container );
 
     // then init the renderer
-    readingSystem.initRenderer(container);
+    rs.initRenderer(container);
 
     // now load the EPUB via the ReadingSytem:Document interface.  Just a local file
     // Since we didn't specify a firstPage config item, it will simply open the document 
     // to the first page, which in this case is the cover page
-    readingSystem.openEPUB("MobyDick.epub");
+    rs.openEPUB("MobyDick.epub");
 
     // check and see if any errors occurred
-    var errorList = readingSystem.getErrorRoot();
+    var errorList = rs.getErrorRoot();
     if (errorList.getCount() > 0) {
         // Uh-oh. Errors occurred
         console.log("Found " + errorList.getCount + " errors occurred");
@@ -51,11 +51,43 @@
         }
     }
 
-    // some navigation examples.  Normally, these would be in some type of event loop
+    // some navigation examples.  Normally, these would be in some type of event loop 
     // but these just show how it might be done
 
     // just go to the next screen
-    readingSystem.nextScreen();
+    rs.nextScreen();
 
     // jump to the end of the document
-    readinSystem.endDocument();
+    rs.endDocument();
+
+    // pop the previous location from the stack and go there (back button behaviour)
+    var loc = rs.getPreviousLocation();
+    rs.gotoLocation( loc );
+
+    // enumerate the NavDoc
+
+    // fetch the metadata
+
+    // manipulate the page layout
+
+
+    // set some text rendering parameters
+
+    // search
+ 
+    // highlights
+    
+    // media overlays
+
+    // exit the app
+
+    // re-open the book with saved settings
+
+
+    
+    
+    
+    
+    
+    
+      
