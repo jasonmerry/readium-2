02ff2cde3467fa4ea24e1b70823b60e2e0aeec1f
Ric Wright <rkwright@geofx.com>
2016-09-11 12:14:03 -0500

    Re-arranged some of the interfaces and changes Document, ReadingSystem for ease of use.  Added section to the demo file to demonstrate NavDoc access.

--- doc/demo/interface-demo.js
+++ doc/demo/interface-demo.js
@@ -37,7 +37,7 @@
     // now load the EPUB via the ReadingSytem:Document interface.  Just a local file
     // Since we didn't specify a firstPage config item, it will simply open the document 
     // to the first page, which in this case is the cover page
-    rs.openEPUB("MobyDick.epub");
+    var epubDoc = rs.openEPUB("MobyDick.epub");
 
     // check and see if any errors occurred
     var errorList = rs.getErrorRoot();
@@ -51,8 +51,10 @@
         }
     }
 
-    // some navigation examples.  Normally, these would be in some type of event loop 
-    // but these just show how it might be done
+    /*
+     * simple navigation examples.  Normally, these would be in some type of event loop 
+     * but these snippets are just show how it might be done
+     */
 
     // just go to the next screen
     rs.nextScreen();
@@ -64,7 +66,72 @@
     var loc = rs.getPreviousLocation();
     rs.gotoLocation( loc );
 
-    // enumerate the NavDoc
+    // enable disable navigation UI if we are at begin/end of document
+    if (rs.isFirstScreen()) {
+        // disable reverse navigation UI 
+    }
+
+    if (rs.isFinalScreen()) {
+        // disable forward navigation UI
+    }
+
+    // fetch the total number of screens - TODO - use promise here
+    rs.getScreenCount( function(nScreens){
+        // display the number of screens in the UI
+    });
+
+    // get the number of the current screen - TODO - use promise here
+    var curLoc = rs.getScreenBegin();
+    rs.getScreenFromLoc( curLoc, function() {
+        // display the current screen index
+    });
+
+    /* 
+     * simple demonstration of NavDoc access
+     */
+
+    // fetch the nav doc element with epub:type of "toc"
+    var navElms = epubDoc.getNavElms();
+
+    // first get the toc (it MUST be the first elm, per interface spec
+    var toc = navElms[0].object;
+
+    // then append that element to the proper location in the UI, e.g.
+    $('#readium-toc-body').append(toc.getNode());
+
+    // alternatively, the app may want to extract the contents of the TOC for their
+    // own presentation of the TOC.  For this the app can use the NavDocItem to recursively 
+    // walk down the tree
+    var walkTOC = function( item ) {
+        for ( var i=0; i<item.getChildCount(); i++ ) {
+            var child = item.getChild(i);
+
+            // do stuff with the child item, then...
+
+            if (child.getChildCount() > 0) {
+                walkTOC(child)
+            }
+        }
+    }
+
+    // just call the recursive function to start the process
+    walkTOC(toc);
+
+    /*
+     * This same process can be performed for the other "standard" nav elements 
+     * The app will need to check for any additional nav elmeents with getOtherNav()  
+     */
+    if (navElms[1] != null) {
+        // process the page-list nav elm
+    }
+    if (navElms[2] != null) {
+        // process the landmarks nav elm
+    }
+
+    // finally, check if there are additional, custom nav elements and process them 
+    for ( var n=3; n < navElms.length; n++ ) {
+        console.log("nav elm name: " + navElms[n].name);
+    }
 
     // fetch the metadata
 
